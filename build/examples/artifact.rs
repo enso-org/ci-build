5135aa9ba3bf1c334fff862b1d7f4bf11dd207f3
Michał W. Urbańczyk <mwu-tow@gazeta.pl>
2022-01-26 05:00:25 +0100

    wip

--- build/examples/artifact.rs
+++ build/examples/artifact.rs
@@ -1,6 +1,7 @@
 use chrono::Duration;
 use reqwest::{Client, ClientBuilder};
 use reqwest::header::HeaderMap;
+use serde::de::DeserializeOwned;
 use ide_ci::github::create_client;
 use ide_ci::prelude::*;
 
@@ -26,7 +27,7 @@ impl Context {
         Url::parse(&url_text).anyhow_err()
     }
 
-    pub fn prepare_client(&self, keep_alive: Option<Duration>) -> Result<Client> {
+    pub fn prepare_json_client(&self, keep_alive: Option<Duration>) -> Result<Client> {
         let mut headers = HeaderMap::new();
         headers.insert(reqwest::header::ACCEPT, iformat!("application/json;api-version={self.api_version}").parse()?);
         // headers.insert(reqwest::header::ACCEPT_ENCODING, "gzip".into());
@@ -41,6 +42,22 @@ impl Context {
 
         ClientBuilder::new().default_headers(headers).user_agent(ide_ci::USER_AGENT).build().anyhow_err()
     }
+
+    // pub fn prepare_bin_client(&self, keep_alive: Option<Duration>) -> Result<Client> {
+    //     let mut headers = HeaderMap::new();
+    //     headers.insert(reqwest::header::ACCEPT, iformat!("application/json;api-version={self.api_version}").parse()?);
+    //     // headers.insert(reqwest::header::ACCEPT_ENCODING, "gzip".into());
+    //     // headers.insert(reqwest::header::ACCEPT, iformat!("application/octet-stream;api-version={api_version}").into());
+    //     headers.insert(reqwest::header::CONTENT_TYPE, "application/json".parse()?);
+    //     headers.insert(reqwest::header::AUTHORIZATION, format!("Bearer {}", self.runtime_token).parse()?);
+    //
+    //     if let Some(keep_alive) = keep_alive {
+    //         headers.insert(reqwest::header::CONNECTION, "Keep-Alive".parse()?);
+    //         headers.insert("Keep-Alive", keep_alive.num_seconds().into());
+    //     }
+    //
+    //     ClientBuilder::new().default_headers(headers).user_agent(ide_ci::USER_AGENT).build().anyhow_err()
+    // }
 }
 
 #[derive(Clone,Debug, Serialize, Deserialize)]
@@ -65,7 +82,7 @@ impl CreateArtifactRequest {
 #[serde(rename_all = "camelCase")] // Sic!
 pub struct CreateArtifactResponse {
     pub container_id: u64,
-    pub size: u64,
+    pub size: i64,
     pub signed_content: String,
     pub file_container_resource_url: String,
     pub r#type: String,
@@ -73,30 +90,86 @@ pub struct CreateArtifactResponse {
     pub url: String,
 }
 
-pub async fn upload_file(path: impl AsRef<Path>, artifact_name: &str) -> Result {
-    // see https://github.com/check-spelling/check-spelling/wiki/%40actions-upload-artifact
-    let context = Context::new()?;
+#[derive(Clone,Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")] // Sic!
+pub struct UploadFileQuery {
+    pub file: String,
+    pub resource_url: Url,
+    pub max_chunk_size: i64,
+    pub continue_on_error: bool,
+}
 
-    let client = context.prepare_client(Some(Duration::seconds(10)))?;
-    let mut headers = HeaderMap::new();
-    // headers.insert(reqwest::header::ACCEPT, iformat!("application/json;api-version={api_version}").into());
-    // headers.insert(reqwest::header::CONTENT_TYPE, "application/json".into());
-    // headers.insert(reqwest::header::AUTHORIZATION, format!("Bearer {}", pat).parse()?);
+// #[derive(Clone,Debug, Serialize, Deserialize)]
+// #[serde(rename_all = "camelCase")] // Sic!
+// pub struct UploadFileResponse {
+//     pub is_success: bool,
+//     pub successful_upload_size: i64,
+//     pub total_size: i64,
+// }
 
-    let query = CreateArtifactRequest::new(artifact_name);
+#[derive(Clone,Debug, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")] // Sic!
+pub struct PatchArtifactSize {
+    pub size: usize,
+}
 
-    let body = serde_json::to_string(&query)?;
-    dbg!(&body);
-    let request = client.post(context.artifact_url()?).body(body).build()?;
+#[derive(Clone,Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")] // Sic!
+pub struct PatchArtifactSizeResponse {
+    pub container_id: u64,
+    pub size: i64,
+    pub signed_content: String,
+    pub r#type: String,
+    pub name: String,
+    pub url: Url,
+    upload_url: Url,
+}
+
+pub async fn execute_dbg<T: DeserializeOwned + std::fmt::Debug>(client: &reqwest::Client, reqeust: reqwest::RequestBuilder) -> Result<T> {
+    let request = reqeust.build()?;
     dbg!(&request);
     let response = client.execute(request).await?;
     dbg!(&response);
     let text = response.text().await?;
     dbg!(&text);
-    //let response = response.json::<CreateArtifactResponse>().await?;
-    let response = serde_json::from_str::<CreateArtifactResponse>(&text)?;
-    dbg!(&response);
+    let deserialized = serde_json::from_str(&text)?;
+    dbg!(&deserialized);
+    Ok(deserialized)
+}
 
+pub async fn upload_file(path: impl AsRef<Path>, artifact_name: &str) -> Result {
+    // see https://github.com/check-spelling/check-spelling/wiki/%40actions-upload-artifact
+    let context = Context::new()?;
+
+    let client = context.prepare_json_client(Some(Duration::seconds(10)))?;
+    // let mut headers = HeaderMap::new();
+    // headers.insert(reqwest::header::ACCEPT, iformat!("application/json;api-version={api_version}").into());
+    // headers.insert(reqwest::header::CONTENT_TYPE, "application/json".into());
+    // headers.insert(reqwest::header::AUTHORIZATION, format!("Bearer {}", pat).parse()?);
+
+    let artifact_url  =context.artifact_url()?;
+
+
+    let query = CreateArtifactRequest::new(artifact_name);
+    let created_artifact: CreateArtifactResponse = execute_dbg(&client, client.post(artifact_url.clone()).json(&query)).await?;
+
+    // Upload file to container.
+    let upload_url = created_artifact.url.clone();
+
+
+    let artifact_path = path.as_ref().file_name().unwrap(); // FIXME
+    let file = std::fs::read_to_string(&path)?;
+
+    let upload_request = client.put(upload_url)
+        .query(&[("itemPath", artifact_path)])
+        .header(reqwest::header::CONTENT_TYPE, "application/octet-stream")
+        .header(reqwest::header::CONTENT_LENGTH, file.len());
+
+    let upload_response:serde_json::Value = execute_dbg(&client, upload_request).await?;
+
+    let patch_request = client.patch(artifact_url.clone()).query(&[("artifactName", artifact_name)]).json(&PatchArtifactSize {size: file.len()});
+
+    let patch_response:serde_json::Value = execute_dbg(&client, patch_request).await?;
 
 //
 //     // see https://github.com/check-spelling/check-spelling/wiki/%40actions-upload-artifact
