c5fc402d17d113f571bfb9bdea1d26e51aa61bb5
Michał W. Urbańczyk <mwu-tow@gazeta.pl>
2022-05-04 11:24:15 +0200

    prettier wrapper

--- build/src/prettier.rs
+++ build/src/prettier.rs
@@ -2,16 +2,15 @@ use crate::prelude::*;
 
 use crate::paths::generated::RepoRoot;
 
-use ide_ci::programs::npx::Npx;
 use ide_ci::programs::Npm;
 
 
-pub fn install_and_run_prettier(repo_root: &RepoRoot, option: &str) -> BoxFuture<'static, Result> {
+pub fn install_and_run_prettier(repo_root: &RepoRoot, script: &str) -> BoxFuture<'static, Result> {
+    let no_args: [&str; 0] = [];
     let prettier_dir = repo_root.build.prettier.as_path();
     let install_cmd = Npm.cmd().map(|mut cmd| cmd.install().current_dir(&prettier_dir).run_ok());
-    let run_cmd = Npx.cmd().map(|mut cmd| {
-        cmd.current_dir(&prettier_dir).arg("prettier").arg(option).arg(repo_root.as_path()).run_ok()
-    });
+    let run_cmd =
+        Npm.cmd().map(|mut cmd| cmd.current_dir(&prettier_dir).run(script, no_args).run_ok());
 
     async move {
         install_cmd?.await?;
@@ -21,9 +20,9 @@ pub fn install_and_run_prettier(repo_root: &RepoRoot, option: &str) -> BoxFuture
 }
 
 pub fn check(repo_root: &RepoRoot) -> BoxFuture<'static, Result> {
-    install_and_run_prettier(repo_root, "--check")
+    install_and_run_prettier(repo_root, "check")
 }
 
 pub fn write(repo_root: &RepoRoot) -> BoxFuture<'static, Result> {
-    install_and_run_prettier(repo_root, "--write")
+    install_and_run_prettier(repo_root, "write")
 }
